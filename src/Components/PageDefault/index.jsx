55126c5f1b10955b0a30372de7349afc13c93e29
Rafael Francucci <rf.francucci@gmail.com>
2023-06-13 20:20:41 -0300

    second

---
+++ src/Components/PageDefault/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function PageDefault({ children }) {
+  return (
+    <div>
+      <h1>Header</h1>
+      {children}
+    </div>
+  );
+}
+
+export default PageDefault;
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { PageDefault } from "../../Components";
+
+function App() {
+  return (
+    <PageDefault>
+      <div>APP</div>
+    </PageDefault>
+  );
+}
+
+export default App;
